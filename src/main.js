dd89cba821b7fae73678df9c6ed126855dd4ee39
Joel <joelisboas@gmail.com>
2022-08-24 11:59:55 -0300

    list commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+
 import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css';
 
